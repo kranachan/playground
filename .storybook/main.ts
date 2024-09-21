49eac6e738e5878bb09086ff1b3f6e4e0e2ecd96
Minakami <necromancer@minakami.land>
2024-09-21 13:30:07 +0800

    chore: change favicon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,20 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { ICON_SVG } from "./favicon";
 import glob from "glob";
 import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/scenes/**/*.stories.@(jsx|tsx)"],
   staticDirs: glob.sync(path.resolve(__dirname, "../submodules/**/public")),
+
+  managerHead: (head) => `
+    ${head}
+    <link rel="icon" type="image/svg+xml" href="data:image/svg+xml, ${encodeURIComponent(ICON_SVG)}" />
+  `,
+
   viteFinal: async (config) => {
     const { mergeConfig } = await import("vite");
+
     return mergeConfig(config, {
       resolve: {
         alias: [
@@ -18,6 +26,7 @@ const config: StorybookConfig = {
       },
     });
   },
+
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
@@ -25,9 +34,11 @@ const config: StorybookConfig = {
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
 };
+
 export default config;
